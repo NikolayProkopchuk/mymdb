90c1c616aac0613d60c9eb5dd6f12717fb4103d5
Nikolay <nikolyand@gmail.com>
2022-10-28 18:18:26 +0300

    initial commit

---
+++ src/main/java/com/prokopchuk/mymdb/MymdbApplication.java
@@ -0,0 +1,13 @@
+package com.prokopchuk.mymdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymdbApplication.class, args);
+    }
+
+}
