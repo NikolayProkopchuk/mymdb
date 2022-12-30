302651d24948c5aaccda04d9f3aa1781ca7785d0
Nikolay <nikolyand@gmail.com>
2022-12-30 13:51:42 +0200

    MYM-9 add run test to ci

--- src/test/java/com/prokopchuk/mymdb/MymdbApplicationTests.java
+++ src/test/java/com/prokopchuk/mymdb/MymdbApplicationTests.java
@@ -1,13 +1,8 @@
 package com.prokopchuk.mymdb;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MymdbApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
